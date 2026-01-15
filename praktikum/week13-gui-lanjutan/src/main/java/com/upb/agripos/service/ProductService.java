ce4fd125eec77ca09572133811a61c28f0a0d6a7
Firly-R <email-mfirlyrmd@gmail.com>
2026-01-15 11:36:33 +0700

    Week13

--- praktikum/week13-gui-lanjutan/src/main/java/com/upb/agripos/service/ProductService.java
+++ praktikum/week13-gui-lanjutan/src/main/java/com/upb/agripos/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.upb.agripos.service;
 
 import com.upb.agripos.dao.ProductDAO;
+import com.upb.agripos.dao.ProductDAOImpl;
 import com.upb.agripos.model.Product;
 import java.util.List;
 
@@ -20,6 +21,9 @@ public class ProductService {
     }
 
     public void deleteProduct(String code) throws Exception {
-        ProductDAO.delete(code); // Memanggil DAO untuk eksekusi SQL
+        // Cast to ProductDAOImpl to access the delete method
+        if (productDAO instanceof ProductDAOImpl) {
+            ((ProductDAOImpl) productDAO).delete(code);
+        }
     }
 }
